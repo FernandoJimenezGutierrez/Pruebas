5b48a92cfc2a88025b41d97a8c29675b63eb6fd2
Fernando <fjgmlama@gmail.com>
2023-02-06 17:18:12 +0100

    Principio

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Probando...</title>
+    <script src="jugadores.js"></script>
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <link rel="stylesheet" href="estilo.css" />
+</head>
+
+<body>
+    <?php
+    include 'JugadoresController.php';
+    ?>
+
+</html>
